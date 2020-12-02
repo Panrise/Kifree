00f0531a8b5b7102778298128e9986e93d0f754d
Panrise <70122578+Panrise@users.noreply.github.com>
2020-12-02 14:42:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 $(document).on("click", "#myButton", function(){
-
-	$.get( "https://taketake.azurewebsites.net/api/HttpTrigger1?code=WRYArh2Z8sXoJM0VyKf0hgzk6UWhXQr2sZbCZTB9LtiBLKZbA/BETg==" ).done( function(value) {
-		alert(value);
-	});
-
-});
+	$.post( "https://taketake.azurewebsites.net/api/HttpTrigger1?code=WRYArh2Z8sXoJM0VyKf0hgzk6UWhXQr2sZbCZTB9LtiBLKZbA/BETg==", "送信したい文章" );
+}
